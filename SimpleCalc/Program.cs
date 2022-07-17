c34d6b4eee63efe9d7c340d452776aec80bb9f1a
Irina-Matisik-1968 <Irina_Matisik@mail.ru>
2022-07-17 15:29:03 +0300

    Add operation

---
+++ SimpleCalc/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input first ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input second ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            double res = a + b;
+            Console.WriteLine("Result = " + res);
+            Console.ReadKey();
+        }
+    }
+}
